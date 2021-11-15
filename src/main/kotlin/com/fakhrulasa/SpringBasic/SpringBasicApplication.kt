d4b49370d652f7b36c6dbfe546fd0597e3500e1d
fakhrul <fakhrul.alam@btracsl.com>
2021-11-15 17:30:07 +0600

    initialize commit

---
+++ src/main/kotlin/com/fakhrulasa/SpringBasic/SpringBasicApplication.kt
@@ -0,0 +1,11 @@
+package com.fakhrulasa.SpringBasic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBasicApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBasicApplication>(*args)
+}
---
+++ src/test/kotlin/com/fakhrulasa/SpringBasic/SpringBasicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fakhrulasa.SpringBasic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
